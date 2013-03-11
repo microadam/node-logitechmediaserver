3e23ba128c5c4d72ad1d61de5e8db69cd32ecd74
Adam Duncan <adam.duncan@clock.co.uk>
2013-03-11 21:24:34 +0000

    Various changes - WIP

--- index.js
+++ index.js
@@ -1,237 +1,273 @@
-var util = require('util');
-var net = require('net');
-var EventEmitter = require('events').EventEmitter;
-var SqueezePlayer = require('./squeezeplayer');
+var util = require('util')
+var net = require('net')
+var EventEmitter = require('events').EventEmitter
+var SqueezePlayer = require('./squeezeplayer')
 
 // Add a trivial method to the net.Stream prototype object to
 // enable debugging during development.  It just appends \n and writes to the stream.
 net.Stream.prototype.writeln = function(s) {
-    // Uncomment the next line to see data as it's written to telnet
-    // console.log("> " + s);
-    this.write(s + "\n");
+  // Uncomment the next line to see data as it's written to telnet
+  // console.log("> " + s)
+  this.write(s + "\n")
 }
+
+// Parse incoming data stream splitting on \n and emitting "line" events
+
+function LineParser(stream) {
+  var self = this
+  self.stream = stream
+  self.buffer = ""
+  self.stream.on("data", function(d) {
+    self.parse(d)
+  })
+}
+util.inherits(LineParser, EventEmitter)
+
+LineParser.prototype.parse = function(data) {
+  this.buffer += data
+  var split = this.buffer.indexOf("\n")
+  while (split > -1) {
+    this.emit('line', this.buffer.slice(0, split))
+    this.buffer = this.buffer.slice(split + 1)
+    split = this.buffer.indexOf("\n")
+  }
+}
+
+
+
 
 // The LogitechMediaServer object is an event emitter with a few properties.
 // After creating it, call .start() and wait for the "registration_finished" event.
-function LogitechMediaServer(address) {
-  var self = this;
-  self.address = address;
-}
-util.inherits(LogitechMediaServer, EventEmitter);
 
+function LogitechMediaServer(address) {
+  this.address = address
+  this.players = {}
+  this.numPlayers = 0
+}
+util.inherits(LogitechMediaServer, EventEmitter)
+
+LogitechMediaServer.prototype.runCmd = function(cmd) {
+  this.telnet.writeln(cmd)
+}
 
 // Start listening to the telnet server provided by Logitech Media Server.
 // I haven't implemented log in with username/password yet - should be easy to do.
 LogitechMediaServer.prototype.start = function() {
-    var self = this;
-    
-    // Listen on port 9090 to self.address
-    self.telnet = net.createConnection(9090, self.address);
-    self.line_parser = new LineParser(self.telnet);
+  var self = this
 
-    // The LineParser just emits a "line" event for each line of data
-    // that the LMS telnet connection emits
-    self.line_parser.on("line", function(data) {
-        // Uncomment the next line to see text lines coming back from telnet
-        // console.log("< " + data.toString().replace(/\n/g,"\\n"));
-        self.handleLine(data);
-    });
+  // Listen on port 9090 to self.address
+  this.telnet = net.createConnection(9090, this.address)
+  this.line_parser = new LineParser(this.telnet)
 
-    // Start things off by asking how many players are connected.
-    // See .handle() - the response to 'player count ?' is how the code
-    // discovers info about all the known players.
-    self.telnet.writeln("player count ?");
+  // The LineParser just emits a "line" event for each line of data
+  // that the LMS telnet connection emits
+  this.line_parser.on("line", function(data) {
+    // Uncomment the next line to see text lines coming back from telnet
+    // console.log("< " + data.toString().replace(/\n/g,"\\n"))
+    self.handleLine(data)
+  })
+
+  // Start things off by asking how many players are connected.
+  this.loadPlayers()
 }
 
-LogitechMediaServer.prototype.handle = function(buffer, keyword, callback) {
-    // If data starts with keyword, call the callback with the remainder, and return true.
-    // Otherwise just return false.
-    // e.g. "player count 3\n", "player count" would call the callback with "3"
-    
-    // seems as if the telnet server URL encodes things
-    var data = decodeURIComponent(buffer.toString());
+LogitechMediaServer.prototype.loadPlayers = function() {
+  var self = this
+  this.getPlayerCount()
+  this.on('playerCountChange', function(count) {
+    // reset in-memory knowledge of players
+    self.numPlayers = count
+    self.players = {}
 
-    // Look for (start of string)(keyword) (data)(end of string)
-    var m = data.match("^" + keyword + "\\s(.*?)$");
-
-    if (m) {
-        // call the callback with the remainder of the string
-        callback(m[1], buffer);
-        return true;
+    // Now issue a "player id" request for each player
+    for (var i = 0; i < self.numPlayers; i++) {
+      this.runCmd('player id ' + i +' ?')
     }
-    return false;
+  })
+  this.on('playerLoaded', function(playerDetails) {
+    self.registerPlayer(playerDetails.index, playerDetails.mac)
+  })
 }
 
-LogitechMediaServer.prototype.handle_with_id = function(buffer, keyword, callback) {
-    // Similar to .handle, but look for a MAC address:
-    // EITHER xx:xx:xx:xx:xx:xx followed by keyword, follwed by data
-    // OR     xx:xx:xx:xx:xx:xx followed by data (keyword should be set to null for this)
-    var self = this;
-
-    // seems as if the telnet server URL encodes things
-    var data = decodeURIComponent(buffer.toString());
-
-    // step through the known players
-    for (mac in self.players) {
-        var player = self.players[mac];
-
-        if (keyword) {
-            // look for (start)(mac) (keyword) (data)(end)
-            var m = data.match("^" + player.id + "\\s" + keyword + "\\s(.*?)$");
-            if (m) {
-                callback(player, m[1], buffer);
-                return true;
-            }
-            // perhaps it's just a line like "00:00:00:00:00:00 stop".  i.e. data is nonexistent
-            // look for (start)(mac) (keyword)(end)
-            var m = data.match("^" + player.id + "\\s" + keyword + "$");
-            if (m) {
-                callback(player, null, buffer);
-                return true;
-            }
-        } else {
-            // look for (start)(mac) (data)(end)
-            var m = data.match("^" + player.id + "\\s(.*?)$");
-            if (m) {
-                callback(player, m[1], buffer);
-                return true;
-            }
-        }
-    };
-    return false;
-
+LogitechMediaServer.prototype.getPlayerCount = function() {
+  this.runCmd('player count ?')
 }
 
 // Passed a player index and a player MAC address, add to in-memory dictionary of players
-LogitechMediaServer.prototype.registerPlayer = function(pnum, pid) {
-    var self = this;
+LogitechMediaServer.prototype.registerPlayer = function(playerIndex, playerMac) {
+  this.players[playerMac] = new SqueezePlayer(this.telnet, playerIndex, playerMac)
 
-    self.players[pid]        = new SqueezePlayer(self.telnet);
-    self.players[pid].id     = pid;
-    self.players[pid].index  = pnum;
+  // Check whether this is the last player we're waiting for, if so emit "registration_finished"
+  if (Object.keys(this.players).length == this.numPlayers) {
+    this.emit('registration_finished')
+    // Can now start listening for all sorts of things!
+    this.runCmd("listen 1")
+  }
+}
 
-    // Check whether this is the last player we're waiting for, if so emit "registration_finished"
-    if (Object.keys(self.players).length == self.numPlayers) {
-        self.emit("registration_finished");
-        // Can now start listening for all sorts of things!
-        self.telnet.writeln("listen 1");
+LogitechMediaServer.prototype.handle = function(buffer, keyword, callback) {
+  // If data starts with keyword, call the callback with the remainder, and return true.
+  // Otherwise just return false.
+  // e.g. "player count 3\n", "player count" would call the callback with "3"
+
+  // seems as if the telnet server URL encodes things
+  var data = decodeURIComponent(buffer.toString())
+
+  // Look for (start of string)(keyword) (data)(end of string)
+  var m = data.match("^" + keyword + "\\s(.*?)$")
+
+  if (m) {
+    return m[1]
+  }
+  return false
+}
+
+LogitechMediaServer.prototype.handle_with_id = function(buffer, keyword, callback) {
+  // Similar to .handle, but look for a MAC address:
+  // EITHER xx:xx:xx:xx:xx:xx followed by keyword, follwed by data
+  // OR     xx:xx:xx:xx:xx:xx followed by data (keyword should be set to null for this)
+  var self = this
+
+  // seems as if the telnet server URL encodes things
+  var data = decodeURIComponent(buffer.toString())
+
+  // step through the known players
+  for (mac in self.players) {
+    var player = self.players[mac]
+    if (keyword) {
+      // look for (start)(mac) (keyword) (data)(end)
+      var m = data.match("^" + player.mac + "\\s" + keyword + "\\s(.*?)$")
+      if (m) {
+        callback(player, m[1], buffer)
+        return true
+      }
+      // perhaps it's just a line like "00:00:00:00:00:00 stop".  i.e. data is nonexistent
+      // look for (start)(mac) (keyword)(end)
+      var m = data.match("^" + player.mac + "\\s" + keyword + "$")
+      if (m) {
+        callback(player, null, buffer)
+        return true
+      }
+    } else {
+      // look for (start)(mac) (data)(end)
+      var m = data.match("^" + player.mac + "\\s(.*?)$")
+      if (m) {
+        callback(player, m[1], buffer)
+        return true
+      }
     }
+  }
+  return false
+
 }
 
-
-// Parse incoming data stream splitting on \n and emitting "line" events
-function LineParser(stream) {
-    var self = this;
-    self.stream = stream;
-    self.buffer = "";
-    self.stream.on("data", function(d) { self.parse(d) });
-}
-util.inherits(LineParser, EventEmitter);
-
-LineParser.prototype.parse = function(data) {
-    this.buffer += data;
-    var split = this.buffer.indexOf("\n");
-    while (split > -1) {
-      this.emit('line', this.buffer.slice(0,split));
-      this.buffer = this.buffer.slice(split+1);
-      split = this.buffer.indexOf("\n");
-    }
-}
 
 // Called with each line received from the telnet connection, this function looks for
 // various commands and acts on them.  Anything unhandled falls out at the bottom
 // (currently gets logged to console), except for unhandled stuff that relates to a player.
 // Those lines get passed to the player object for handling.
 LogitechMediaServer.prototype.handleLine = function(buffer) {
-    var self = this;
-    var handled = false;
+  var self = this
+  var handled = false
+  var params = false
 
-    // Guts of this function is pretty much a list of commands and callbacks.
-    // Could definitely be made more efficient, or a bit DRYer, but it's just a bunch of string comparisons.
-    
-    // "player count" response is what kicks things off in the first place (see .start())
-    if (self.handle(buffer, "player count", function(params, buffer) {
-        // reset in-memory knowledge of players
-        self.numPlayers = parseInt(params[0]);
-        self.players = {};
+  // Guts of this function is pretty much a list of commands and callbacks.
+  // Could definitely be made more efficient, or a bit DRYer, but it's just a bunch of string comparisons.
 
-        // Now issue a "player id" request for each player
-        for (var p=0; p<self.numPlayers; p++) {
-            self.telnet.writeln("player id " + p + " ?");
-        }
-    })) { handled=true } ;
+  // "player count" response is what kicks things off in the first place (see .start())
+  params = self.handle(buffer, "player count");
+  if (params) {
+    self.emit('playerCountChange', parseInt(params[0]))
+    return true
+  }
 
-    // This response is received for each player, so store 'em in memory as a dic
-    if (self.handle(buffer, "player id", function(params, b) {
-        params = params.split(" ");
-        var pnum = parseInt(params[0]);
-        var pid  = params[1];
+  // This response is received for each player, so store 'em in memory as a dic
+  params = self.handle(buffer, "player id")
+  if (params) {
+    params = params.split(" ")
+    var playerIndex = parseInt(params[0])
+    , playerMac = params[1]
 
-        self.registerPlayer(pnum, pid);
+    self.emit('playerLoaded', {index: playerIndex, mac: playerMac})
+    return true
+  }
 
-        // Now that we know the id & mac, ask for more info.  name and signalstrength, also power state
-        self.telnet.writeln(pid + " signalstrength ?");
-        self.telnet.writeln(pid + " name ?");
-        self.telnet.writeln(pid + " power ?");
-        self.telnet.writeln(pid + " mixer volume ?");
-    })) { handled = true } ;
+  // Just handle the "listen" response (LMS should just respond with 'listen 1' at the beginning)
+  if (self.handle(buffer, "listen", function() {})) {
+    handled = true
+  }
 
-    // Just handle the "listen" response (LMS should just respond with 'listen 1' at the beginning)
-    if (self.handle(buffer, "listen", function() {})) { handled = true };
+  // ~~~~~~~~~~~~~~ keywords below here are those which are associated with an individual player ~~~~~~~~~~~~~~~~~~
 
-    // ~~~~~~~~~~~~~~ keywords below here are those which are associated with an individual player ~~~~~~~~~~~~~~~~~~
+  if (self.handle_with_id(buffer, "signalstrength", function(player, params, b) {
+    player.setProperty("signalstrength", parseInt(params))
+  })) {
+    handled = true
+  }
 
-    if (self.handle_with_id(buffer, "signalstrength", function(player, params, b) {
-        player.setProperty("signalstrength", parseInt(params));
-    })) { handled = true } ;
+  if (self.handle_with_id(buffer, "power", function(player, params, b) {
+    player.setProperty("power", parseInt(params))
 
-    if (self.handle_with_id(buffer, "power", function(player, params, b) {
-        player.setProperty("power", parseInt(params));
-
-        if (player.power == 1) {
-            // Wait a tiny bit while player is powering up and then ask what state the player is in
-            setTimeout(function() { player.runTelnetCmd("mode ?") }, 1500);
-        } else {
-            player.setProperty("mode", "off");
-        }
-    })) { handled = true } ;
-
-    if (self.handle_with_id(buffer, "name", function(player, params, b) {
-        player.setProperty("name", params);
-    })) { handled = true };
-
-    if (self.handle_with_id(buffer, "current_title", function(player, params, b) {
-        player.setProperty("current_title", params);
-    })) { handled = true };
-
-    if (self.handle_with_id(buffer, "mode", function(player, params, b) {
-        player.setProperty("mode", params);  // "play", "stop" or "pause"
-    })) { handled = true };
-
-    if (self.handle_with_id(buffer, "play", function(player, params, b) {
-        player.setProperty("mode", "play");
-        // player has started playing something.  Let's find out what!
-        player.runTelnetCmd("current_title ?");
-    })) { handled = true };
-
-    if (self.handle_with_id(buffer, "stop", function(player, params, b) {
-        player.setProperty("mode", "stop");
-    })) { handled = true };
-
-    if (self.handle_with_id(buffer, "pause", function(player, params, b) {
-        player.setProperty("mode", "pause");
-    })) { handled = true };
-
-    if (!handled) {
-        // handle any string received that starts with an id and isn't handled yet by passing events
-        // to the player objects.
-        if (!self.handle_with_id(buffer, null, function(player, params, b) {
-            player.handleServerData(params, b);
-        })) {
-            // anything else, just log to console for now.  Could be an event of its own.
-            console.log("unhandled line", decodeURIComponent(buffer.toString()));
-        }
+    if (player.power == 1) {
+      // Wait a tiny bit while player is powering up and then ask what state the player is in
+      setTimeout(function() {
+        player.runTelnetCmd("mode ?")
+      }, 1500)
+    } else {
+      player.setProperty("mode", "off")
     }
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "name", function(player, params, b) {
+    player.setProperty("name", params)
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "current_title", function(player, params, b) {
+    player.setProperty("current_title", params)
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "mode", function(player, params, b) {
+    player.setProperty("mode", params) // "play", "stop" or "pause"
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "play", function(player, params, b) {
+    player.setProperty("mode", "play")
+    // player has started playing something.  Let's find out what!
+    player.runTelnetCmd("current_title ?")
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "stop", function(player, params, b) {
+    player.setProperty("mode", "stop")
+  })) {
+    handled = true
+  }
+
+  if (self.handle_with_id(buffer, "pause", function(player, params, b) {
+    player.setProperty("mode", "pause")
+  })) {
+    handled = true
+  }
+
+  if (!handled) {
+    // handle any string received that starts with an id and isn't handled yet by passing events
+    // to the player objects.
+    if (!self.handle_with_id(buffer, null, function(player, params, b) {
+      player.handleResponse(params, b)
+    })) {
+      // anything else, just log to console for now.  Could be an event of its own.
+      console.log("unhandled line", decodeURIComponent(buffer.toString()))
+    }
+  }
 }
 
-module.exports = LogitechMediaServer;
+module.exports = LogitechMediaServer
--- squeezeplayer.js
+++ squeezeplayer.js
@@ -1,79 +1,80 @@
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
+var util = require('util')
+var EventEmitter = require('events').EventEmitter
 
 function startsWith(search, s) {
-    return s.substr(0,search.length) == search;
+  return s.substr(0, search.length) == search
 }
 
-function SqueezePlayer(telnet) {
-  var self = this;
-  this.telnet = telnet;
-  this.volume = 0;
-  // periodically check this player's parameters
-  setInterval(function() {
-    self.runTelnetCmd("mixer volume ?");
-    self.runTelnetCmd("signalstrength ?");
-    self.runTelnetCmd("power ?");    
-  }, 30 * 1000);
-}
-util.inherits(SqueezePlayer, EventEmitter);
+function SqueezePlayer(telnet, index, mac) {
+  this.telnet = telnet
+  this.index = index
+  this.mac = mac
+  this.name = null
+  this.volume = null
 
-SqueezePlayer.prototype.runTelnetCmd = function(cmdstring) {
-    this.telnet.writeln(this.id + " " + cmdstring);
+  this.runCmd('name ?')
+  this.runCmd('mixer volume ?')
 }
+util.inherits(SqueezePlayer, EventEmitter)
 
-SqueezePlayer.prototype.handleServerData = function(strEvent, raw_buffer) {
-    var self = this;
-    if (startsWith("mixer volume", strEvent)) {
-        var v = strEvent.match(/^mixer volume\s(.*?)$/)[1];
-        // incremental change
-        if (startsWith("+", v) || startsWith("-", v)) {
-            self.volume += parseInt(v);
-        }
-        // explicit value
-        else {
-            self.volume = parseInt(v);
-        }
-        this.emit("volume", v);
-    } else {
-        this.emit("logitech_event", strEvent);
-    }
+SqueezePlayer.prototype.runCmd = function(cmd) {
+  this.telnet.writeln(this.mac + ' ' + cmd)
 }
 
 SqueezePlayer.prototype.switchOff = function() {
-    this.runTelnetCmd("power 0");
+  this.runCmd('power 0')
 }
 
 SqueezePlayer.prototype.switchOn = function() {
-    this.runTelnetCmd("power 1");
+  this.runCmd('power 1')
 }
 
-SqueezePlayer.prototype.setProperty = function(property, state) {
-    this[property] = state;
-    this.emit(property, state);
+SqueezePlayer.prototype.setVolume = function(volume) {
+  this.runCmd('mixer volume ' + volume)
 }
 
-SqueezePlayer.prototype.getNoiseLevel = function() {
-    var nl = this.volume;
-    if (this.mode == "stop" || this.mode == "pause" || this.mode == "off" || this.power == 0) {
-        nl = 0;
+SqueezePlayer.prototype.setProperty = function(property, value) {
+  this[property] = value
+  this.emit(property, value)
+  if (this.name && this.volume) {
+    this.emit('update', this.get())
+  }
+}
+
+SqueezePlayer.prototype.get = function() {
+  var
+    self = this
+  , object = {}
+  
+  Object.keys(this).forEach(function(property) {
+    if (["telnet", "_events"].indexOf(property) == -1) {
+      object[property] = self[property]
     }
-    return nl;
+  })
+  return object
 }
 
-SqueezePlayer.prototype.inspect = function() {
-    // Convenience method for debugging/logging.
-    // Return self but without certain lengthy sub-objects.
-    var self = this;
-    var x = {};
-    Object.keys(self).forEach(function(k) {
-        if (["telnet", "_events"].indexOf(k) == -1) {
-            x[k] = self[k];
-        }
-    });
-    x.noise_level = self.getNoiseLevel();
-    return x;
+SqueezePlayer.prototype.handleResponse = function(cmdString) {
+
+  if (startsWith("mixer volume", cmdString)) {
+    var volume = cmdString.match(/^mixer volume\s(.*?)$/)[1]
+    // incremental change
+    if (startsWith("+", volume) || startsWith("-", volume)) {
+      this.setProperty('volume', this.volume + parseInt(volume))
+      // explicit volume  
+    } else {
+      this.setProperty('volume', parseInt(volume))
+    }
+    return true
+  }
+
+  if (startsWith("name", cmdString)) {
+    var name = cmdString.match(/^name\s(.*?)$/)[1]
+    this.setProperty('name', name)
+    return true
+  }
+
 }
 
-module.exports = SqueezePlayer;
 
+module.exports = SqueezePlayer
