577f8c5ed059e643a0690021afe2dddf52bbe4f1
Adam Duncan <adam.duncan@clock.co.uk>
2013-03-13 21:38:48 +0000

    Handling connecting / disconnecting of clients

--- LogitechMediaServer.js
+++ LogitechMediaServer.js
@@ -14,7 +14,7 @@ function LogitechMediaServer(address, port) {
   this.port = port
   this.players = {}
   this.numPlayers = 0
-  this.syncGroups = []
+  this.syncGroups = null
 }
 util.inherits(LogitechMediaServer, EventEmitter)
 
@@ -42,18 +42,45 @@ LogitechMediaServer.prototype.registerEvents = function() {
     }
   })
 
-  this.cli.on('playerLoaded', function(playerDetails) {
-    self.registerPlayer(playerDetails.index, playerDetails.mac)
+  this.cli.on('playerLoaded', function(macAddress) {
+    self.registerPlayer(macAddress)
+  })
+
+  this.cli.on('playerConnected', function(macAddress) {
+    self.numPlayers++
+    self.registerPlayer(macAddress)
+  })
+
+  this.cli.on('playerDisconnect', function(macAddress) {
+    delete self.players[macAddress]
+    self.numPlayers--
+    self.retrieveSyncGroups()
   })
 
   this.cli.on('syncGroupsChanged', function() {
-    this.retrieveSyncGroups()
+    self.retrieveSyncGroups()
   })
 
   this.cli.on('syncGroupsLoaded', function(syncGroups) {
+    var playersInGroups = []
+    self.syncGroups = []
+
     if (syncGroups) {
       self.syncGroups = syncGroups
+      // group all grouped players so we can determine which arent grouped
+      self.syncGroups.forEach(function(group) {
+        playersInGroups = playersInGroups.concat(group)
+      })
     }
+
+    // all all ungrouped players to their own group
+    for(mac in self.players) {
+      var player = self.players[mac]
+      if (playersInGroups.indexOf(player.mac) === -1) {
+        self.syncGroups.push([player.mac])
+      }
+    }    
+    
   })
 
   this.cli.once('syncGroupsLoaded', function() {
@@ -89,8 +116,8 @@ LogitechMediaServer.prototype.getSyncGroups = function() {
 }
 
 // Passed a player index and a player MAC address, add to in-memory dictionary of players
-LogitechMediaServer.prototype.registerPlayer = function(playerIndex, playerMac) {
-  this.players[playerMac] = new Player(this.cli, playerIndex, playerMac)
+LogitechMediaServer.prototype.registerPlayer = function(macAddress) {
+  this.players[macAddress] = new Player(this.cli, macAddress)
 
   // Check whether this is the last player we're waiting for
   if (Object.keys(this.players).length === this.numPlayers) {
